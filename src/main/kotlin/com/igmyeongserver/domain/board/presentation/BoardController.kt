41939f81f45ab2fe049a904ae80cb77c4380922c
Jueuunn7 <wndmsrla1234@gmail.com>
2025-06-05 12:27:23 +0900

    feat: swagger

--- src/main/kotlin/com/igmyeongserver/domain/board/presentation/BoardController.kt
+++ src/main/kotlin/com/igmyeongserver/domain/board/presentation/BoardController.kt
@@ -3,6 +3,7 @@ package com.igmyeongserver.domain.board.presentation
 import com.igmyeongserver.domain.board.presentation.dto.req.CreateBoardReqDto
 import com.igmyeongserver.domain.board.presentation.dto.res.BoardListResDto
 import com.igmyeongserver.domain.board.service.BoardService
+import io.swagger.v3.oas.annotations.Operation
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -11,12 +12,15 @@ import java.net.URI
 @RestController
 @RequestMapping("/board")
 class BoardController(private val boardService: BoardService) {
+
     @GetMapping
+    @Operation(summary = "Get all boards")
     fun getBoardList(): ResponseEntity<List<BoardListResDto>> {
         return ResponseEntity.ok(boardService.getBoardList())
     }
 
     @PostMapping
+    @Operation(summary = "Create a new board")
     fun createBoard(
         @RequestBody dto: CreateBoardReqDto,
         request: HttpServletRequest,
